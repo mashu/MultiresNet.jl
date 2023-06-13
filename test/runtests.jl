a730d695993d2c5190b2ba282720324f61da972c
Mateusz Kaduk <mateusz.kaduk@gmail.com>
2023-06-13 12:23:16 +0200

    tests/runtests.jl: Add test for MultiresBlock to match output of fading_forward() from PyTorch.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,25 @@ using MultiresNet
 using Test
 
 @testset "MultiresNet.jl" begin
-    # Write your tests here.
+    channels = 3
+    depth = 4
+    seqlen = 4
+    kernel = 4
+    x = Array{Float64, 3}(undef, 1, 3, 4)
+    x[1,1,:] = [1.0, 2.0, 3.0, 4.0]
+    x[1,2,:] = [5.0, 6.0, 7.0, 8.0]
+    x[1,3,:] = [9.0, 10.0, 11.0, 12.0]
+    h = Array{Float64, 3}(undef, 3, 1, 4)
+    h[1,1,:] = [1.0, 2.0, 3.0, 4.0]
+    h[2,1,:] = [5.0, 6.0, 7.0, 8.0]
+    h[3,1,:] = [9.0, 10.0, 11.0, 12.0]
+    w = Array{Float64, 2}(undef, channels, depth+2)
+    w[1,:] .= [1.0, 2.0, 3.0, 5.0, 6.0, 7.0]
+    w[2,:] .= [1.0, 2.0, 3.0, 6.0, 7.0, 8.0]
+    w[3,:] .= [3.0, 4.0, 5.0, 7.0, 8.0, 9.0]
+    seq_block = MultiresNet.MultiresBlock(h, h, w)
+    @test seq_block != nothing
+    output = seq_block(MultiresNet.reverse_dims(x))[:,:,1]'
+    @test output ≈ [1071.0 2940.0 6121.0 10153.0; 71360.0 148037.0 290440.0 439288.0; 1.394145e6 2.826942e6 5.562655e6 8.346751e6]
 end
+
